91794c13053967a4fb4172b13222813c9abc3763
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-21 00:01:56 +0300

    "Removed useEffect and axios imports, and uncommented useState import in Header.tsx"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+import React, { useState } from 'react';
 interface MenuItem {
   id: number;
   title: string;
