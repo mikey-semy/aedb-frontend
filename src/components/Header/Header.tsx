6e728b728ea4c56abb756090759d851363db3a32
MikhailTo <toshkin.mikhail@yandex.ru>
2024-10-02 23:01:16 +0300

    "Updated styles for header, manuals, and global components; added fixed top class to Header component"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import NavigationLocal from './NavigationLocal';
 
 const Header: React.FC = () => {
   return (
-    <header className="app-header">
+    <header className="app-header app-header--fixed-top">
         <Logo />
         <NavIcon />
         <NavigationLocal />
