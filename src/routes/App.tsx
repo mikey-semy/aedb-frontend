5e67d3e487eb482b8abb816d9470f4d0c1819aed
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-10-21 08:59:54 +0000

    remove import icon

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { Outlet } from "react-router-dom";
-
-import favicon from '../public/favicon.ico';
 
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
@@ -10,14 +8,6 @@ import Footer from '../components/Footer/Footer';
 
 
 const App: React.FC = () => {
-
-  useEffect(() => {
-    const link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
-    if (link) {
-      link.href = favicon;
-    }
-  }, []);
-
   return (
     <div className="app-container">
       <Header />
