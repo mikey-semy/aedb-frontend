c161b9984ba0a551bc734712808d5780449e2ee5
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-12-03 04:38:49 +0000

    fix: login

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from "react-router-dom";
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { SidebarProvider, ThemeProvider, useTheme, ContentDataProvider } from '@/contexts';
 import { Header, Sidebar, Footer, Content } from '@/components';
@@ -9,25 +9,21 @@ import { Login } from '@/pages';
 
 const AppContent: React.FC = () => {
   const { isDark } = useTheme();
-
+  const navigate = useNavigate();
   const token = localStorage.getItem('token');
-    
-  if (!token) {
-    return ( 
-      <StyledThemeProvider theme={isDark ? DarkTheme : LightTheme}>
-        <GlobalStyles />
-        <ResetStyles />
-        <Variables />
-        <Login />
-      </StyledThemeProvider>
-    );
-  }
+  
+  useEffect(() => {
+    if (!token) {
+      navigate('/login'); // Перенаправляем на страницу входа, если токен отсутствует
+    }
+  }, [token, navigate]);
 
   return (
     <StyledThemeProvider theme={isDark ? DarkTheme : LightTheme}>
-      <GlobalStyles />
-      <ResetStyles />
-      <Variables />
+    <GlobalStyles />
+    <ResetStyles />
+    <Variables />
+    {token ? (
       <AppContainer>
         <Sidebar />
         <MainContainer>
@@ -40,8 +36,9 @@ const AppContent: React.FC = () => {
           <Footer />
         </MainContainer>
       </AppContainer>
-    </StyledThemeProvider>
-  );
+    ) : null}
+  </StyledThemeProvider>
+);
 };
 
 const App: React.FC = () => {
