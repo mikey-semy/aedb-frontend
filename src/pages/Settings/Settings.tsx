c77d5c9e866730f92e4464a7ac0ff5b190426f72
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-01-04 00:43:09 +0300

    Update Settings.tsx

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useContentData } from '@/contexts';
 import { Tabs, Search, Profile, Empty } from '@/components';
 
