6467c1e91f9a3ddec39f4ede522b027c28e060dc
Mike <34812500+mikey-semy@users.noreply.github.com>
2025-01-04 01:11:41 +0300

    Update UserMenu.tsx

--- src/components/User/UserMenu/UserMenu.tsx
+++ src/components/User/UserMenu/UserMenu.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UserMenuContainer, Menu, MenuItem } from './user-menu.styles';
 import { Avatar, ProfileButton, LogoutButton } from '@/components';
+import { useAuth } from '@/contexts';
+import { getUserProfile } from '../Profile/Profile.api';
 
 const UserMenu: React.FC = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { token } = useAuth();
+    const [userData, setUserData] = useState(null);
+    
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const data = await getUserProfile(token);
+                setUserData(data);
+            } catch (err) {
+                setError('Ошибка при загрузке профиля');
+            }
+        };
+
+        fetchProfile();
+    }, [token]);
+
     return (
         <UserMenuContainer>
             <Avatar
+                UserData={userData}
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
             />
             {isMenuOpen && (
